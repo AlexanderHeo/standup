eb52a6ff63b139557d07ce17f8bcfb13e06b80e4
Alexander Heo <alexheo.webdev@gmail.com>
2022-01-05 15:22:17 -0800

    feat: basic randomizer app - complete

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import getDate from './getDate';
 
 const App = () => {
-  return <h1>Morning Stand UP</h1>;
+  const [randomized, setRandomized] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [lateAdditions, setLateAdditions] = useState([]);
+  const [input, setInput] = useState('');
+  const [display, setDisplay] = useState(false);
+  const [added, setAdded] = useState(false);
+  const [lateAdded, setLateAdded] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleAddButton = (e) => {
+    e.preventDefault();
+
+    if (display) {
+      const lateAddition = [...lateAdditions];
+      lateAddition.push(input);
+      setLateAdded(true);
+      setLateAdditions(lateAddition);
+    }
+    const membersCopy = [...members];
+    membersCopy.push(input);
+    setAdded(true);
+    setMembers(membersCopy);
+    setInput('');
+  };
+
+  const disp = () => {
+    randomizer(members);
+  };
+
+  const randomizer = (members) => {
+    const membersCopy = [...members];
+    let len = membersCopy.length,
+      temp,
+      randomIndex;
+    while (len) {
+      randomIndex = Math.floor(Math.random() * len--);
+      temp = membersCopy[len];
+      membersCopy[len] = membersCopy[randomIndex];
+      membersCopy[randomIndex] = temp;
+    }
+    setRandomized(membersCopy);
+    setDisplay(true);
+  };
+
+  const todayIs = getDate();
+  return (
+    <div className='wrapper'>
+      <section className='date-section'>
+        <h1>Morning Stand UP</h1>
+        <h2>
+          {todayIs.month} {todayIs.day} {todayIs.date}, {todayIs.year}
+        </h2>
+      </section>
+      <section className='dayof-qotd'>
+        <div className='day'>National ----- Day</div>
+        <div className='qotd'>QotD: Margins and Padding - px or rems?</div>
+      </section>
+      <main className='container'>
+        <section className='inputBox'>
+          <form onSubmit={(e) => handleAddButton(e1)}>
+            <div className='buttonContainer'>
+              <input
+                className='input'
+                onChange={(e) => handleInputChange(e)}
+                value={input}
+              />
+              <button
+                type='submit'
+                className='addButton'
+                onClick={(e) => handleAddButton(e)}
+              >
+                Add
+              </button>
+            </div>
+          </form>
+          {added && (
+            <ul>
+              {members.map((member, index) => (
+                <li key={index}>{member}</li>
+              ))}
+            </ul>
+          )}
+        </section>
+
+        <section className='outputBox'>
+          <div className='buttonContainer'>
+            <button className='randomButton' onClick={disp}>
+              Randomize
+            </button>
+          </div>
+          <div className='randomList'>
+            {display && (
+              <ol className='orderedList'>
+                {randomized.map((member, index) => (
+                  <div key={index}>
+                    <li className='name'>{member}</li>
+                  </div>
+                ))}
+                {lateAdditions.map((member, index) => (
+                  <li key={index} onClick={(e) => handleMemberClick(e)}>
+                    {member}
+                  </li>
+                ))}
+              </ol>
+            )}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/components/getDate.js
@@ -0,0 +1,80 @@
+const getDate = () => {
+  const newDate = new Date();
+  const dNum = newDate.getDay();
+  const mNum = newDate.getMonth();
+  const date = newDate.getDate();
+  const year = newDate.getFullYear();
+
+  let day, month;
+  switch (dNum) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+  }
+  switch (mNum) {
+    case 0:
+      month = 'January';
+      break;
+    case 1:
+      month = 'February';
+      break;
+    case 2:
+      month = 'March';
+      break;
+    case 3:
+      month = 'April';
+      break;
+    case 4:
+      month = 'May';
+      break;
+    case 5:
+      month = 'June';
+      break;
+    case 6:
+      month = 'July';
+      break;
+    case 7:
+      month = 'August';
+      break;
+    case 8:
+      month = 'September';
+      break;
+    case 9:
+      month = 'October';
+      break;
+    case 10:
+      month = 'November';
+      break;
+    case 11:
+      month = 'December';
+      break;
+    default:
+      month = 'Unknown';
+  }
+  return {
+    month,
+    day,
+    date,
+    year,
+  };
+};
+
+export default getDate;
